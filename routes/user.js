3e88a868aa2fe88bc39912143bda731275b5482b
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-01-03 14:54:22 -0500

    check for user, create, hash password

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,19 @@ const express = require('express');
 const router = express.Router();
 // Express Validator for forms
 const { check, validationResult } = require('express-validator');
+// Require in user model
+const User = require('../models/User')
+// Bring in Gravatar
+const gravatar = require('gravatar');
+// Bring in bcrypt
+const bcrypt = require('bcryptjs');
 
 // @route GET api/users
 // @desc: Test
 // @access Public
-router.get('/', (req,res) => res.send('User Route'));
+router.get('/', (req,res) => 
+    res.send('User Route')
+);
 
 //POST
 router.post('/', [
@@ -21,36 +29,55 @@ router.post('/', [
         // Checks that password is present and that it has more than the minimum characters.
     check('password', 'Please use a password with 6 or more characters!')
         .isLength({ min: 6 })
-],(req,res) => {
+],
+    async (req,res) => {
     // console.log(req.body)
     // Declare constant of errors set to the validation result
     const errors = validationResult(req)
     // If errors, return 400 error and error array in JSON
     if(!errors.isEmpty()) {
         return res.status(400).json( { errors: errors.array() } );
-    }
+    } 
     try {
         // See if User already exists
         let user = await User.findOne({ email });
-        if(user) {
-            res.status(400).json({ errors: [{msg: 'User already exists' }] });
-        }
-   
+            //IF user exists, send error
+            if(user) {
+                res.status(400).json({ errors: [{msg: 'User already exists' }] });
+            }
         const { name, email, password } = req.body;
-
-
-        //IF user exists, send error
         // GET User's gravatar
-        // Encrypt password using BCRYPT
-        // Return JSON Web token
+        // Gravatar settings
+        const avatar = gravatar.url(email, {
+            // img size
+            s: '200',
+            // img rating
+            r: 'pg',
+            // Default if no gravatar exists
+            d: 'mm'
+        });
+        // creates new user, BUT does not save yet
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+        // Need to encrypt password before saving it to db
+        // create salt
+        const salt = await bcrypt.genSalt(10);
+        // hash password. Takes in plain text password and salt
+        user.password = await bcrypt.hash(password, salt);
+        // Save to db
+        // Returns promise so await
+        await user.save();
+        // TO DO: Return JSON Web token
 
-        res.send('User Route')
-
+        res.send('User Registered')
     } catch(err) {
         console.error(err.message);
         res.status(500).send('Server error');
     }
 });
-
 
 module.exports = router;
