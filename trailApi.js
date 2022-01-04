776b8e4e7a6c0f96112358a15caf7a448b8446eb
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-01-04 13:11:50 -0500

    trail data

--- trailApi.js
+++ trailApi.js
@@ -6,15 +6,17 @@ var TRAIL_KEY = process.env.TRAIL_API_KEY
 
 function getLocation() {
     
+    // Zip Code entered by User
     var ZIPCODE = zipSearch.value;
 
+    // If none entered, alert user
     if (!ZIPCODE) {
         window.alert('Please enter a zip code!');
     }
 
     var locationUrl =`https://www.zipcodeapi.com/rest/${ZIP_KEY}/info.json/${ZIPCODE}/degrees`
     
-        console.log(ZIPCODE);
+    // TEST: console.log(ZIPCODE);
     
     fetch(locationUrl).then(function (response) {
         if (!response.ok) {
@@ -29,6 +31,7 @@ function getLocation() {
             window.alert("this is not a valid zipcode!");
         }
 
+        // API DATA:
         var LAT = data.lat;
         var LON = data.lng;
 
@@ -47,11 +50,30 @@ function searchTrails (LAT, LON) {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com",
-            "x-rapidapi-key": "SIGN-UP-FOR-KEY"
+            "x-rapidapi-key": TRAIL_KEY
         }
     })
     .then(response => {
         console.log(response);
+
+        const returnedTrailData = data.data;
+        // API DATA:
+        const trailData = returnedTrailData.map((data) => ({
+            ID = data.id,
+            LAT = data.lat,
+            LON = data.lng,
+            NAME = data.name,
+            URL = data.url, 
+            LENGTH = data.length,
+            DESC = data.description,
+            DIRECTIONS = data.directions,
+            CITY = data.city,
+            REG = data.region,
+            DIF = data.difficulty,
+            FEAT = data.features,
+            RATING = data.rating,
+            PIC = data.thumbnail
+        }));
     })
     .catch(err => {
         console.error(err);
